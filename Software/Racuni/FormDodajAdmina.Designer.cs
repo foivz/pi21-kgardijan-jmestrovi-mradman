cb339798822c067b420c1c856586efb8841c6fd5
kgardijan <kgardijan@foi.hr>
2021-06-28 00:28:20 +0200

    Funkcionalnost izdavanja računa
    Račun se izdaje te se korisniku šalje na mail u pdf obliku uz dodatak QR koda. QR kod prikazan u aplikaciji i u pdf obliku

---
+++ Software/Racuni/FormDodajAdmina.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Funkcionalnost_prijave
+{
+    partial class FormDodajAdmina
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.textBoxImePrezime = new System.Windows.Forms.TextBox();
+            this.textBoxLozinka = new System.Windows.Forms.TextBox();
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonOdustani = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Korisničko ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ime i prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lozinka:";
+            // 
+            // textBoxKorisnickoIme
+            // 
+            this.textBoxKorisnickoIme.Location = new System.Drawing.Point(16, 61);
+            this.textBoxKorisnickoIme.Name = "textBoxKorisnickoIme";
+            this.textBoxKorisnickoIme.Size = new System.Drawing.Size(190, 22);
+            this.textBoxKorisnickoIme.TabIndex = 3;
+            // 
+            // textBoxImePrezime
+            // 
+            this.textBoxImePrezime.Location = new System.Drawing.Point(16, 215);
+            this.textBoxImePrezime.Name = "textBoxImePrezime";
+            this.textBoxImePrezime.Size = new System.Drawing.Size(190, 22);
+            this.textBoxImePrezime.TabIndex = 4;
+            // 
+            // textBoxLozinka
+            // 
+            this.textBoxLozinka.Location = new System.Drawing.Point(16, 145);
+            this.textBoxLozinka.Name = "textBoxLozinka";
+            this.textBoxLozinka.Size = new System.Drawing.Size(190, 22);
+            this.textBoxLozinka.TabIndex = 5;
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(124, 257);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(81, 29);
+            this.buttonDodaj.TabIndex = 6;
+            this.buttonDodaj.Text = "Dodaj";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.buttonDodaj_Click);
+            // 
+            // buttonOdustani
+            // 
+            this.buttonOdustani.Location = new System.Drawing.Point(27, 257);
+            this.buttonOdustani.Name = "buttonOdustani";
+            this.buttonOdustani.Size = new System.Drawing.Size(81, 29);
+            this.buttonOdustani.TabIndex = 7;
+            this.buttonOdustani.Text = "Odustani";
+            this.buttonOdustani.UseVisualStyleBackColor = true;
+            this.buttonOdustani.Click += new System.EventHandler(this.buttonOdustani_Click);
+            // 
+            // FormDodajAdmina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 320);
+            this.Controls.Add(this.buttonOdustani);
+            this.Controls.Add(this.buttonDodaj);
+            this.Controls.Add(this.textBoxLozinka);
+            this.Controls.Add(this.textBoxImePrezime);
+            this.Controls.Add(this.textBoxKorisnickoIme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDodajAdmina";
+            this.Text = "Dodaj admina";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxKorisnickoIme;
+        private System.Windows.Forms.TextBox textBoxImePrezime;
+        private System.Windows.Forms.TextBox textBoxLozinka;
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonOdustani;
+    }
+}
