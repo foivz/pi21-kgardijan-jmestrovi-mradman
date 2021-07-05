38128216f6a943c3e1c34a13341aa5d0a9f4da79
kgardijan <kgardijan@foi.hr>
2021-07-05 18:14:02 +0200

    QR Code omogućeno učitavanje u mBanking

--- Software/RestoranAPK/FormStatistika.cs
+++ Software/RestoranAPK/FormStatistika.cs
@@ -35,48 +35,34 @@ namespace Funkcionalnost_prijave
 
         private void FormStatistika_Load(object sender, EventArgs e)
         {
-            comboBoxStatus.Visible = false;
+            
             entities.Orders.Load();
             orderBindingSource.DataSource = entities.Orders.Local;
-            DohvatiStatuse();
-            comboBoxStatus.DataSource = ListaStatusa;
+            
+            
 
             
             
         }
 
-        private object DohvatiNarudzbe()
-        {
-            using (var context = new EntitiesOrder())
-            {
-                ListaNarudzbi = new List<Order>();
-                foreach (var item in context.Orders)
-                {
-                    if (item.Status != "Zavrseno")
-                    {
-                        ListaNarudzbi.Add(item);
-                    }
+        //private object DohvatiNarudzbe()
+        //{
+        //    using (var context = new EntitiesOrder())
+        //    {
+        //        ListaNarudzbi = new List<Order>();
+        //        foreach (var item in context.Orders)
+        //        {
+        //            if (item.Status != "Zavrseno")
+        //            {
+        //                ListaNarudzbi.Add(item);
+        //            }
 
-                }
-                return ListaNarudzbi;
-            }
-        }
+        //        }
+        //        return ListaNarudzbi;
+        //    }
+        //}
 
-        private void DohvatiStatuse()
-        {
-            ListaStatusa = new List<string>();
-            using (var context = new EntitiesOrder())
-            {
-                foreach (var item in context.OrderStatus)
-                {
-                    if (item.Status != "Zavrseno")
-                    {
-                        ListaStatusa.Add(item.Status.ToString());
-                    }
-                }
-            }
-        }
-
+       
 
         private void btnStat_Click(object sender, EventArgs e)
         {
