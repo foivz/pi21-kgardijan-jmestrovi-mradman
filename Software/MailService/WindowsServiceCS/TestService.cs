02d3853a13d257472840e636aeff5a17a1d8d38a
Mateo <mradman@foi.hr>
2021-07-06 18:21:14 +0200

    MailService promjena

---
+++ Software/MailService/WindowsServiceCS/TestService.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.IO;
+using System.Threading;
+using System.Configuration;
+using System.Net.Mail;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace WindowsServiceCS
+{
+    public partial class TestService : ServiceBase
+    {
+        public TestService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.WriteToFile("Simple Service started {0}");
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            this.WriteToFile("Simple Service stopped {0}");
+            this.Schedular.Dispose();
+        }
+
+        private Timer Schedular;
+
+        public void ScheduleService()
+        {
+            try
+            {
+                Schedular = new Timer(new TimerCallback(SchedularCallback));
+                string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
+                this.WriteToFile("Simple Service Mode: " + mode + " {0}");
+
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse(System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"]);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {0} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                this.WriteToFile("Simple Service scheduled to run after: " + schedule + " {0}");
+
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                Schedular.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Simple Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+                //Stop the Windows Service.
+                using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+
+private void SchedularCallback(object e)
+{
+    try
+    {
+        DataTable dt = new DataTable();
+        string query = "SELECT ImeGosta, EmailGosta FROM Reservation WHERE DATEPART(DAY, DatumVrijeme) = @Day AND DATEPART(MONTH, DatumVrijeme) = @Month AND DATEPART(YEAR, DatumVrijeme) = @Year";
+
+
+        
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand(query))
+            {
+                cmd.Connection = con;
+                cmd.Parameters.AddWithValue("@Day", DateTime.Today.Day + 1 );
+                cmd.Parameters.AddWithValue("@Month", DateTime.Today.Month);
+                cmd.Parameters.AddWithValue("@Year", DateTime.Today.Year);
+
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    sda.Fill(dt);
+                }
+            }
+        }
+
+        foreach (DataRow row in dt.Rows)
+        {
+            string name = row["ImeGosta"].ToString();
+            string email = row["EmailGosta"].ToString();
+            WriteToFile("Trying to send email to: " + name + " " + email);
+
+            using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+            {
+                        client.EnableSsl = true;
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential("piprojektmail@gmail.com", "piprojekttest123");
+                        MailMessage msg = new MailMessage();
+                        msg.To.Add(email);
+                        msg.From = new MailAddress("piprojekttest123@gmail.com");
+                        msg.Subject = "Podsjetnik na rezervaciju";
+                        msg.Body = "Poštovani, "+ name + " ovim putem Vas podsjećamo na Vašu sutrašnju rezervaciju. Veselimo se Vašem dolasku!";
+                        client.Send(msg);
+                        WriteToFile("Email sent successfully to: " + name + " " + email);
+            }
+        }
+        this.ScheduleService();
+    }
+    catch (Exception ex)
+    {
+        WriteToFile("Simple Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+        //Stop the Windows Service.
+        using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController("SimpleService"))
+        {
+            serviceController.Stop();
+        }
+    }
+}
+
+        private void WriteToFile(string text)
+        {
+            string path = "C:\\ServiceLog.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                writer.Close();
+            }
+        }
+    }
+}
